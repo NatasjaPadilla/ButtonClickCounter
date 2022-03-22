9d604b13c4890e365a360b05b3fbd3f8d5440aa2
Natasja <86358047+NatasjaPadilla@users.noreply.github.com>
2022-03-22 10:16:22 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.buttonclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setText("BUTTON CLICKED:" + ++count);
+            }
+        });
+    }
+}
